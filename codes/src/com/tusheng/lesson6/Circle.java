bd607443903448c877f206c9551e30a235fd1cb9
Zhu Tao <zhutao2015@qq.com>
2015-04-14 14:22:26 +0800

    new ppt

--- codes/src/com/tusheng/lesson6/Circle.java
+++ codes/src/com/tusheng/lesson6/Circle.java
@@ -1,5 +1,10 @@
 package com.tusheng.lesson6;
 
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Vector;
+
 public class Circle{
 	public double calculateArea(int radius){
 		return 3.14 * radius * radius;
@@ -9,7 +14,18 @@ public class Circle{
 	public static void main(String argv[]){
 		Circle c = new Circle();
 		int r = 20;
-		System.out.printf("Area of Circle(%d) is %.2f", r, c.calculateArea(r));
+		System.out.printf("Area of Circle(%d) is %.2f\n", r, c.calculateArea(r));
 //		System.out.print("Area of Circle(" + r + ") is " + c.calculateArea(r));
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		arr.add(10);
+		HashMap<String, Integer> x = new HashMap<String, Integer>();
+		x.put("hello", 10);
+		System.out.println(x.get("hello"));
+		String ss = "123";
+		//ss.setCharAt(0, '9');
+		StringBuffer s = new StringBuffer("123");
+		System.out.println(s);
+		s.setCharAt(0, '9');
+		System.out.println(s);
 	}
 }
--- lesson6/Circle.java
+++ lesson6/Circle.java
@@ -1,4 +1,4 @@
-
+package com.zhutao;
 public class Circle{
     public double calculateArea(int radius){
         return 3.14 * radius * radius;
