aa16e4ba2e99147796184a0bd26522d9b4794741
Zhu Tao <zhutao2015@qq.com>
2015-04-17 09:05:21 +0800

    update

--- codes/src/com/tusheng/lesson7/Main.java
+++ codes/src/com/tusheng/lesson7/Main.java
@@ -4,9 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		StringMatch s = new StringMatch("hello");
+		StringMatch s = new StringMatch("ccbbbaadef");
 		char c = 'l';
 		System.out.printf("number of %c: %d\n", c, s.numberOfOccurence(c));
+		System.out.println("max sub str is:" + s.getMaxSubStr(2));
+		System.out.println("max sub str is:" + s.getMaxSubStr(3));
+		System.out.println("max sub str is:" + s.getMaxSubStr(1));
+		System.out.println("max sub str is:" + s.getMaxSubStr(4));
+		
 	}
 
 }
--- codes/src/com/tusheng/lesson7/StringMatch.java
+++ codes/src/com/tusheng/lesson7/StringMatch.java
@@ -1,5 +1,7 @@
 package com.tusheng.lesson7;
 
+import java.util.ArrayList;
+
 public class StringMatch {
 	private String str;
 	
@@ -17,7 +19,31 @@ public class StringMatch {
 		return cnt;
 	}
 	
+	private boolean isSame(String s){
+		if (s.isEmpty()) return false;
+		char c = s.charAt(0);
+		for(int i=1; i<s.length();i ++){
+			if (c != s.charAt(i))
+				return false;
+		}
+		return true;
+	}
+	
 	public String getMaxSubStr(int len){
-		return "";
+		if (len > str.length()) return "";
+		ArrayList<String> substrs = new ArrayList<String>();
+		for(int i=0; i+len<str.length(); i++){
+			String sub = str.substring(i, i+len);
+			if (isSame(sub)){
+				substrs.add(sub);
+			}
+		}
+		if (substrs.isEmpty()) return "";
+		String maxstr = substrs.get(0);
+		for(String s: substrs){
+			if (maxstr.compareTo(s) > 0)
+				maxstr = s;
+		}
+		return maxstr;
 	}
 }
